acaa2114b795e6d253695026db56647ffb5315fe
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2025-02-07 11:21:16 +0000

    adsad

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Person {
+    public:
+        string name;
+        int age;
+
+};
+
+
+class Student : public Person{
+    public:
+        int studentID;
+};
+
+
+
+
+
+int main(){
+
+}
